23ec660d7eb4fc75ad612e2cfd74a6a8fa4f219b
Miriam Zusin <miriamzusin@gmail.com>
2023-05-13 00:33:58 +0300

    refactoring

--- src/core/ui/RoundSlider.tsx
+++ src/core/ui/RoundSlider.tsx
@@ -1,13 +1,13 @@
-import { Slider } from './Slider';
 import { IUserSettings } from '../interfaces';
 import { store } from '../data/store';
 import { Provider } from 'react-redux';
+import { SVG } from './SVG';
 
 export const RoundSlider = (props: IUserSettings) => {
 
     return (
         <Provider store={ store }>
-           <Slider {...props} />
+           <SVG {...props} />
         </Provider>
     )
 };
--- src/core/ui/Slider.tsx
+++ src/core/ui/Slider.tsx
@@ -1,41 +1,14 @@
 import {
     MouseEvent as ReactMouseEvent,
-    TouchEvent as ReactTouchEvent, useEffect,
+    TouchEvent as ReactTouchEvent,
     useRef,
 } from 'react';
 import { Pointer } from './Pointer';
-import { getEllipseSegment, getPointerPosition, getSVGCenter, getSVGSize } from '../domain/svg-provider';
-import { Vector2 } from 'mz-math';
+import { getPointerPosition } from '../domain/svg-provider';
 import { useAppDispatch, useAppSelector } from '../data/store';
-import { getNumber, getString } from '../domain/common';
-import {
-    DEFAULT_BG_COLOR,
-    DEFAULT_POINTER_RX, DEFAULT_POINTER_RY,
-    DEFAULT_STROKE_WIDTH,
-    DEFAULT_SVG_RX,
-    DEFAULT_SVG_RY
-} from '../domain/defaults';
-import { normalizeAngles } from '../domain/angles-provider';
-import { IStatePointer, IUserSettings } from '../interfaces';
 import { sliderActions } from '../data/slider-slice';
 
-const getMaxPointer = (pointers: IStatePointer[]) : Vector2 => {
-    let maxX = -Infinity;
-    let maxY = -Infinity;
-
-    for(const pointer of pointers){
-        const [rx, ry] =  pointer.pointerRadii;
-        maxX = Math.max(maxX, rx);
-        maxY = Math.max(maxY, ry);
-    }
-
-    return [
-        maxX,
-        maxY,
-    ];
-};
-
-export const Slider = (props: IUserSettings) => {
+export const Slider = () => {
 
     const svgRef = useRef<SVGSVGElement>(null);
     const sliderRef = useRef<SVGPathElement>(null);
@@ -57,72 +30,6 @@ export const Slider = (props: IUserSettings) => {
     const [ svgWidth, svgHeight ] = svgSize;
 
     const dispatch = useAppDispatch();
-
-    /**
-     * Combine user settings with the defaults,
-     * and init slider state properties.
-     */
-    useEffect(() => {
-        // Define initial SVG circle/ellipse radii.
-        const svgRadii: Vector2 = [
-            getNumber(props.rx, DEFAULT_SVG_RX),
-            getNumber(props.ry, DEFAULT_SVG_RY)
-        ];
-
-        // Define ellipse/circle segment using angles defined in degrees.
-        const [startAngleDegrees, endAngleDegrees] = normalizeAngles(props.startAngleDegrees, props.endAngleDegrees);
-        const angles: Vector2 = [startAngleDegrees, endAngleDegrees];
-
-        const strokeWidth = getNumber(props.strokeWidth, DEFAULT_STROKE_WIDTH);
-        const bgColor = getString(props.bgColor, DEFAULT_BG_COLOR);
-
-        // Convert user provided pointers settings to the actual state pointers' definition.
-        const _pointers = props.pointers ? props.pointers.map(pointer => {
-            return {
-                pointerRadii: [
-                    getNumber(pointer.rx, DEFAULT_POINTER_RX),
-                    getNumber(pointer.ry, DEFAULT_POINTER_RY),
-                ]
-            } as IStatePointer;
-        }) : pointers;
-
-        const maxPointer: Vector2 = getMaxPointer(_pointers);
-        const svgSize: Vector2 = getSVGSize(svgRadii, maxPointer, strokeWidth);
-        const svgCenter: Vector2 = getSVGCenter(svgRadii, maxPointer, strokeWidth);
-
-        const { sliderStartPoint, sliderEndPoint, largeArcFlag } = getEllipseSegment(
-            startAngleDegrees,
-            endAngleDegrees,
-            svgRadii,
-            maxPointer,
-            strokeWidth
-        );
-
-        const pointerPositions: Vector2[] = _pointers.map(pointer => {
-            return sliderStartPoint
-        });
-
-        dispatch(
-            sliderActions.initUserSettings({
-                // svg look & feel properties ---------
-                svgRadii,
-                angles,
-                strokeWidth,
-                bgColor,
-
-                // pointers ----------------------------
-                pointers: _pointers,
-                pointerPositions,
-
-                // calculated properties ---------------
-                svgSize,
-                svgCenter,
-                sliderStartPoint,
-                sliderEndPoint,
-                largeArcFlag,
-            })
-        );
-    }, []);
 
     const onValueChange = (evt: MouseEvent | ReactMouseEvent | TouchEvent | ReactTouchEvent) => {
 
