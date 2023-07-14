c335bfaec821e7c302ce8402e3c58f65a5d9cfcd
Miriam Zusin <miriamzusin@gmail.com>
2023-07-14 15:41:36 +0300

    docs update

--- src/docs/client-side/js/home-page/home-page.tsx
+++ src/docs/client-side/js/home-page/home-page.tsx
@@ -4,9 +4,9 @@ import * as React from 'react';
 import { RoundSlider } from '../../../../core';
 import { useState } from 'react';
 
-const initSlider1 = () => {
-    const $colorSlider = document.getElementById('color-slider') as HTMLElement;
-    if(!$colorSlider) return;
+const initColorSlider = () => {
+    const $slider = document.getElementById('color-slider') as HTMLElement;
+    if(!$slider) return;
 
     const Component = () => {
 
@@ -26,7 +26,7 @@ const initSlider1 = () => {
 
                 pathBgColor={ '#d0d0d0' }
                 pathThickness={ 5 }
-                pathInnerBgColor={ 'url(#gradient)' }
+                pathInnerBgColor={ 'url(#color-slider-gradient)' }
                 connectionBgColor={ '#939191' }
 
                 pointerBgColor={ '#fff' }
@@ -59,7 +59,7 @@ const initSlider1 = () => {
 
                 SvgDefs={
                     <>
-                        <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <linearGradient id="color-slider-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
                             <stop offset="0%" stopColor={ `hsl(${ value }, 100%, 40%)` } />
                             <stop offset="100%" stopColor={ `hsl(${ value }, 50%, 20%)` } />
                         </linearGradient>
@@ -69,8 +69,74 @@ const initSlider1 = () => {
         );
     };
 
-    const colorSlider = ReactDOM.createRoot($colorSlider);
-    colorSlider.render(
+    const slider = ReactDOM.createRoot($slider);
+    slider.render(
+        <React.StrictMode>
+            <Component />
+        </React.StrictMode>
+    );
+};
+
+const initHorseshoeSlider = () => {
+    const $slider = document.getElementById('color-slider') as HTMLElement;
+    if(!$slider) return;
+
+    const Component = () => {
+
+        return (
+            <RoundSlider
+                animateOnClick={ true }
+                pathStartAngle={ 150 }
+                pathEndAngle={ 30 }
+
+                pathBgColor={ '#2C2C2F' }
+                pathThickness={ 15 }
+                connectionBgColor={ 'url(#horseshoe-slider-gradient)' }
+
+                pointerBgColor={ '#fff' }
+                pointerBgColorSelected={ '#eeeeee' }
+                pointerRadius={ 20 }
+
+                enableTicks={ true }
+                ticksCount={ 100 }
+                ticksGroupSize={ 10 }
+                ticksWidth={ 1 }
+                ticksDistanceToPanel={ 5 }
+                tickValuesDistance={ 20 }
+                tickValuesColor={ '#e1e1e1' }
+
+                textColor={ '#fff' }
+                textFontSize={ 24 }
+                textSuffix={ '“' }
+                textPrefix={ ' '}
+
+                min={ 0 }
+                max={ 100 }
+
+                pointers={[
+                    {
+                        value: 0,
+                    },
+                    {
+                        value: 50,
+                    }
+                ]}
+
+                SvgDefs={
+                    <>
+                        <linearGradient id="horseshoe-slider-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                            <stop offset="0%" stopColor="#2d95a8" />
+                            <stop offset="50%" stopColor="#67CD67" />
+                            <stop offset="100%" stopColor="#cccc68" />
+                        </linearGradient>
+                    </>
+                }
+            />
+        );
+    };
+
+    const slider = ReactDOM.createRoot($slider);
+    slider.render(
         <React.StrictMode>
             <Component />
         </React.StrictMode>
@@ -82,5 +148,6 @@ export const initHomePage = () => {
         hljs.highlightAll();
     }
 
-    initSlider1();
+    initColorSlider();
+    initHorseshoeSlider();
 };
